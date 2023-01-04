74098a08214f358a117c794865329dd817d3f3c1
Baylak <mongush.baylak17@gmail.com>
2023-01-04 21:21:37 +0300

    Add input strings and refactore code

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 # importing all required libraries
+import asyncio
+
 from telethon import TelegramClient
 
-# get your api_id, api_hash
-api_id = 'some_api_id'
-api_hash = 'some_api_hash'
-message = 'some_message'
-
-# your phone number
-phone = '+123456789'
+api_id = input('Enter your API ID: ')
+api_hash = input('Enter your API Hash: ')
+message = input('Enter your message: ')
+phone = input('Enter your phone number: ')
 
 # creating a telegram session and assigningA
-client = TelegramClient('your_username', api_id, api_hash)
+your_username = input("Enter your Telegram username: ")
 
-# connecting and building the session
-client.connect()
 
-# ensuring you're authorized
-if not client.is_user_authorized():
-    # sending code request
-    client.send_code_request(phone)
+async def telegram_async():
+    async with TelegramClient(your_username, api_id, api_hash) as client:
 
-    # signing in the client
-    client.sign_in(phone, input('Enter the code: '))
+        # connecting and building the session
+        await client.connect()
 
-try:
-    # sending message using telegram client
-    client.send_file('some_username', 'file_location', caption=message)
-except Exception as e:
-    # error handling
-    print(e)
+        # ensuring you're authorized
+        if not await client.is_user_authorized():
+            # sending code request
+            await client.send_code_request(phone)
 
-# disconnecting the telegram session
-client.disconnect()
+            # signing in the client
+            await client.sign_in(phone, input('Enter the code: '))
+
+        try:
+            # username of the target
+            username_target = input('Enter the username of the person you want to send message to: ')
+            # sending message using username
+            sending_file = input('Enter the location of the file you want to send: ')
+
+            # sending message using telegram client
+            await client.send_file(username_target, sending_file, caption=message)
+        except Exception as e:
+            # error handling
+            print(e)
+
+        # disconnecting the telegram session
+        await client.disconnect()
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(telegram_async())
+
+
+if __name__ == '__main__':
+    main()
